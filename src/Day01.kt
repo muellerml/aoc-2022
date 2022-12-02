9f82694807e970d1c70e745e67899195b35ce0bf
Michael Müller <michael.mueller.90@gmail.com>
2022-12-02 23:17:13 +0100

    add solution for day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,49 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.foldIndexed(Calories(0, 0)) { index, calories, addition ->
+            when (val addToInt = addition.toIntOrNull()) {
+                null -> if (calories.max < calories.current) {
+                    println("Highest value found for index $index")
+                    calories.copy(max = calories.current, current = 0)
+                } else Calories(calories.max, 0)
+                else -> calories.copy(current = calories.current + addToInt)
+            }
+        }.max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves: MutableList<Elf> = mutableListOf()
+        input.foldIndexed(Elf(0)) { index, elf, calories: String ->
+            val addToCalories = calories.toIntOrNull()
+            println(index)
+            when {
+                addToCalories == null -> {
+                    elves.add(elf)
+                    Elf(0)
+                }
+                index == (input.size - 1) -> {
+                    elves.add(elf.copy(calories = elf.calories + addToCalories))
+                    Elf(0)
+                };
+                else -> elf.copy(calories = elf.calories + addToCalories)
+            }
+        }
+        return elves.sortedByDescending { it.calories }.take(3).sumOf { it.calories }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val result = part1(testInput)
+    check(result == 24000)
+
+    val result2 = part2(testInput)
+    check(result2 == 45000)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part1: " + part1(input))
+    println("Part2: " + part2(input))
 }
+
+
+data class Calories(val max: Int, val current: Int)
+data class Elf(val calories: Int)
